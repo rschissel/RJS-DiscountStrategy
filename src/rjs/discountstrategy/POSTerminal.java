98b1f831fa3630d6f69212781d5672e2b3cc98dd
ryan-schissel <ryan.schissel@yahoo.com>
2016-10-13 14:33:57 -0500

    Added VideoOutput

--- src/rjs/discountstrategy/POSTerminal.java
+++ src/rjs/discountstrategy/POSTerminal.java
@@ -12,7 +12,7 @@ package rjs.discountstrategy;
 public class POSTerminal {
     private DataStore ds;
     private Receipt receipt;
-    private ReceiptOutput output = new ConsoleOutput();
+    private ReceiptOutput output = new VideoOutput();
     private OutputManager om = new OutputManager(output);
     
     public final void createSale(String customerID, DataStore ds) {
---
+++ src/rjs/discountstrategy/VideoOutput.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rjs.discountstrategy;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ryan Schissel
+ */
+public class VideoOutput implements ReceiptOutput{
+
+    @Override
+    public void outputReceipt(Receipt receipt) {
+        JOptionPane.showMessageDialog(null, receipt.getReceiptContents());
+    }
+    
+}
